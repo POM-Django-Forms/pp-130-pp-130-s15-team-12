d996c29033e57ed78603938b259e5bf3fea09e08
koskin <kostia.zeykin@gmail>
2025-06-18 15:11:50 +0300

    added form book_form.html to book/templates + added path to form to urls.py

--- library/book/urls.py
+++ library/book/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import create_book
 
 urlpatterns = [
     path('user/', views.book_list_view, name='user_book_list'),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('update/<int:book_id>/', views.update_book_view, name='update_book'),
     path('delete/<int:book_id>/', views.delete_book_view, name='delete_book'),
     path('provided/<int:user_id>/', views.user_books_view, name='user_books'),
+    path('add/', create_book, name='create_book'),
 ]
